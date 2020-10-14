a65acddfdd5bb0091e6927adcc3f5369eaed12ec
andrew <AndrewsafwatSamuel@gmail.com>
2020-10-14 21:48:18 +0200

    1-implemented_cleanArchitecture

--- app/src/main/java/com/example/recipesandroidapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/recipesandroidapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.recipesandroidapp
+package com.example.recipesandroidapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recipesandroidapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
