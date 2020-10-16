2aaed426dbc4d8364897b659c37daed6c0401c1f
andrew <AndrewsafwatSamuel@gmail.com>
2020-10-16 02:14:12 +0200

    implemented ConnectivityListener

--- app/src/main/java/com/example/recipesandroidapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/recipesandroidapp/presentation/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.example.recipesandroidapp.presentation
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import com.example.recipesandroidapp.R
-import com.example.recipesandroidapp.domain.dataSources.databaseGateway.databaseInstance
-import com.example.recipesandroidapp.entities.InAppRecipe
-import com.example.recipesandroidapp.entities.Recipe
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
-import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ConnectivityListener(this).observe(this) {
+            if (it == true) offline_textView.hide() else offline_textView.show()
+        }
     }
 }
--- app/src/main/java/com/example/recipesandroidapp/presentation/engine.kt
+++ app/src/main/java/com/example/recipesandroidapp/presentation/engine.kt
@@ -1,28 +1,112 @@
 package com.example.recipesandroidapp.presentation
 
+import android.annotation.TargetApi
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.*
+import android.net.wifi.p2p.WifiP2pManager
+import android.os.Build
 import android.view.View
 import android.widget.ImageView
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.LiveData
 import com.bumptech.glide.Glide
 
-fun ImageView.loadPhoto(url:String?)=Glide
+fun ImageView.loadPhoto(url: String?) = Glide
     .with(context)
-    .load(url?:"")
+    .load(url ?: "")
     .into(this)
 
-fun View.show(){
+fun View.show() {
     visibility = View.VISIBLE
 }
 
-fun View.hide(){
+fun View.hide() {
     visibility = View.GONE
 }
 
-fun View.enable(){
+fun View.enable() {
     isEnabled = true
 }
 
-fun View.disable(){
+fun View.disable() {
     isEnabled = false
+}
+
+@Suppress("DEPRECATION")
+class ConnectivityListener(private val context: Context) : LiveData<Boolean>() {
+    private val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    private lateinit var callbackManager: ConnectivityManager.NetworkCallback
+
+    override fun onActive() {
+        super.onActive()
+        updateConnection()
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.N -> {
+                connectivityManager.registerDefaultNetworkCallback(connectivityManagerCallback())
+            }
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP -> {
+                lollipopNetworkRequester()
+            }
+            else -> {
+                context.registerReceiver(
+                    networkReceiver,
+                    IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+                )
+            }
+        }
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP -> connectivityManager.unregisterNetworkCallback(
+                connectivityManagerCallback()
+            )
+            else -> context.unregisterReceiver(networkReceiver)
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun lollipopNetworkRequester() {
+        val requestBuilder = NetworkRequest.Builder()
+            .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+        connectivityManager.registerNetworkCallback(requestBuilder.build(), callbackManager)
+    }
+
+    private fun connectivityManagerCallback(): ConnectivityManager.NetworkCallback {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) throw IllegalAccessError("Error")
+        callbackManager = @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        object : ConnectivityManager.NetworkCallback() {
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                postValue(false)
+            }
+
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                postValue(true)
+            }
+
+        }
+        return callbackManager
+    }
+
+    private val networkReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            updateConnection()
+        }
+    }
+
+    private fun updateConnection() {
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        postValue(activeNetwork?.isConnected == true)
+    }
 }
 
 
